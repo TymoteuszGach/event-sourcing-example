79b97e56cac98a9f73c1e5623a4ed8d66cc43fbc
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2021-09-30 00:13:15 +0200

    fixed response

--- src/examples/simple/users/get/route.ts
+++ src/examples/simple/users/get/route.ts
@@ -13,12 +13,10 @@ export const route = (router: Router) =>
         const result = await getUser(request.params.email)
 
         if (result.isError) {
-          console.log(result)
           response.sendStatus(404)
           return
         }
         response.send(result.value)
-        response.sendStatus(200)
       } catch (error) {
         next(error)
       }
